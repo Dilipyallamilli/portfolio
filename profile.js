a2c6a0bfb9924b4b70ceef921ad9f2a508a7daca
Dilipyallamilli <dilipkumaryallamilli@gmail.com>
2024-12-20 10:52:00 +0530

    dilip

---
+++ profile.js
@@ -0,0 +1,7 @@
+
+let checkbox = document.getElementById("checkbox");
+let smallmenu = document.getElementById("smallmenu");
+checkbox.addEventListener('click', function() {
+
+    smallmenu.classList.toggle('d-none');
+})
